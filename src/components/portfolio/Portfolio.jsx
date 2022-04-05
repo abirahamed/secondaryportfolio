a1b50ef675e560a3ceb7291852ebec795620a53a
Abir Ahamed <abirahamed281102@gmail.com>
2022-04-05 19:23:12 +0600

    Done..

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
+
+export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Fetured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "branding",
+      title: "Branding",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "fetured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+
+      default:
+        setData(featuredPortfolio);
+        break;
+    }
+  }, [selected]);
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => {
+          return (
+            <PortfolioList
+              title={item.title}
+              active={selected === item.id}
+              setSelected={setSelected}
+              id={item.id}
+            />
+          );
+        })}
+      </ul>
+      <div className="container">
+        {data.map((d) => {
+          return (
+            <div className="item">
+              <img src={d.img} alt="" />
+              <h3>{d.title}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+8801963708512</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>abirahamed281102@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div
+            className="hamburger"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            <span className="line-1"></span>
+            <span className="line-2"></span>
+            <span className="line-3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
